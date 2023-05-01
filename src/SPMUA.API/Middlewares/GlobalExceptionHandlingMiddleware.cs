714aa7190b0e6fb3027da28bf67ea73f3d1b5335
Stefan Ivković <ivkovics@outlook.com>
2023-05-01 12:08:37 +0200

    Add global exception handling middleware.

---
+++ src/SPMUA.API/Middlewares/GlobalExceptionHandlingMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace SPMUA.API.Middlewares
+{
+    public class GlobalExceptionHandlingMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+			try
+			{
+				await next(context);
+			}
+			catch
+			{
+				context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+				ProblemDetails details = new()
+				{
+					Type = "Server error",
+					Title = "Internal Server Error",
+					Detail = "An error occurred while processing the request."
+				};
+
+				await context.Response.WriteAsJsonAsync<ProblemDetails>(details);
+			}
+        }
+    }
+}
--- src/SPMUA.API/Program.cs
+++ src/SPMUA.API/Program.cs
@@ -1,3 +1,5 @@
+using SPMUA.API.Middlewares;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<GlobalExceptionHandlingMiddleware>();
 
 var app = builder.Build();
 
@@ -19,6 +22,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseMiddleware<GlobalExceptionHandlingMiddleware>();
 
 app.MapControllers();
 
