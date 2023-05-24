25af4c050ce469a88c07d465907b6139d3e2cbab
Stefan Ivković <ivkovics@outlook.com>
2023-05-24 22:48:10 +0200

    Add email templates

--- src/SPMUA.Model/Models/EmailTemplate.cs
+++ src/SPMUA.Model/Models/EmailTemplate.cs
@@ -5,7 +5,7 @@ namespace SPMUA.Model.Models
     public class EmailTemplate
     {
         public int EmailTemplateId { get; set; }
-        [MaxLength(30)]
+        [MaxLength(50)]
         public string EmailTemplateName { get; set; } = null!;
         [MaxLength(100)]
         public string EmailTemplateTitle { get; set; } = null!;
