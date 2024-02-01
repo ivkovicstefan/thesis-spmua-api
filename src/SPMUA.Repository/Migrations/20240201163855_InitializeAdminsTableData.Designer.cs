f3b14627b99191df745b24ff7ac0662473b288dd
Stefan Ivković <ivkovics@outlook.com>
2024-02-01 17:41:26 +0100

    Add Admins table default data

--- src/SPMUA.Repository/Migrations/20240201163855_InitializeAdminsTableData.Designer.cs
+++ src/SPMUA.Repository/Migrations/20240201163855_InitializeAdminsTableData.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SPMUA.Repository.Data;
 
@@ -11,9 +12,11 @@ using SPMUA.Repository.Data;
 namespace SPMUA.Repository.Migrations
 {
     [DbContext(typeof(SpmuaDbContext))]
-    partial class SpmuaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240201163855_InitializeAdminsTableData")]
+    partial class InitializeAdminsTableData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/SPMUA.Repository/Migrations/20240201163855_InitializeAdminsTableData.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SPMUA.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitializeAdminsTableData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Admins",
+                columns: new[] { "AdminId", "AdminEmail", "AdminFirstName", "AdminLastName", "PasswordHash" },
+                values: new object[] { 1, "ivkovics@outlook.com", "System", "Admin", "$2a$11$O49GrWrVzkBO3jvUa9fGY.hh12Clbk6.HumaYdEwi5abLa8R3Q3ca" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Admins",
+                keyColumn: "AdminId",
+                keyValue: 1);
+        }
+    }
+}
